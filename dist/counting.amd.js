b22b820db78072a8cf19e3d2a17de128fd6a0682
Alan Plum <me@pluma.io>
2015-03-31 11:31:05 +0200

    Rebuilt.

--- dist/counting.amd.js
+++ dist/counting.amd.js
@@ -2,19 +2,24 @@ define(function(require, exports, module) {
 /*jshint es3: true */
 /*globals module */
 'use strict';
-function counting(offset, fn) {
+function counting(offset, increment, fn) {
   if (typeof offset === 'function') {
-    fn = offset;
+    increment = offset;
     offset = undefined;
+  }
+  if (typeof increment === 'function') {
+    fn = increment;
+    increment = undefined;
   }
   function wrapper() {
     /*jshint validthis: true */
     var args = Array.prototype.slice.call(arguments);
     args.unshift(wrapper.count);
-    wrapper.count += 1;
+    wrapper.count += wrapper.increment;
     return fn.apply(this, args);
   }
   wrapper.count = offset || 0;
+  wrapper.increment = increment || 1;
   return wrapper;
 }
 
--- dist/counting.globals.js
+++ dist/counting.globals.js
@@ -4,19 +4,24 @@ var module = {exports: {}};
 /*jshint es3: true */
 /*globals module */
 'use strict';
-function counting(offset, fn) {
+function counting(offset, increment, fn) {
   if (typeof offset === 'function') {
-    fn = offset;
+    increment = offset;
     offset = undefined;
+  }
+  if (typeof increment === 'function') {
+    fn = increment;
+    increment = undefined;
   }
   function wrapper() {
     /*jshint validthis: true */
     var args = Array.prototype.slice.call(arguments);
     args.unshift(wrapper.count);
-    wrapper.count += 1;
+    wrapper.count += wrapper.increment;
     return fn.apply(this, args);
   }
   wrapper.count = offset || 0;
+  wrapper.increment = increment || 1;
   return wrapper;
 }
 
